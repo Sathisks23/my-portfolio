0e430ef218c0ad78c375ccd8c4cc9d0c0cffad29
Sathishks23 <ahalyaddckap@gmail.com>
2024-12-06 15:35:12 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-center">
-      <h1>My portofolio</h1>
+      <h1>Welcome MY Portfolio</h1>
     </div>
   );
 }
