a5a267f4f03f871f9d18ede8cbe61151d1f5a871
Sathishks23 <ahalyaddckap@gmail.com>
2024-12-07 16:32:55 +0530

    meta data added

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <meta name="google-site-verification" content="uvZ3FIqlGxeTrYfKvjDBMVI980FIDsfUvG3Lj0lCvls" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
