dcb4e4cf2d916a02f420d1a418673ec10b6e0445
Sathishks23 <ahalyaddckap@gmail.com>
2024-12-08 12:21:11 +0530

    google seo testing

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://sathishdiaries.vercel.app', // Replace with your site's URL
+    generateRobotsTxt: true, // Generate a robots.txt file
+    // Optional: Add more sitemap options if needed
+  };
+  
