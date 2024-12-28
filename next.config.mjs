07d71a4cab5f7171906ad410dbc5eeb003bc988c
Sathishks23 <ahalyaddckap@gmail.com>
2024-12-28 15:40:38 +0530

    for deployemt change

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-})
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['eslint:recommended', 'next'],
-  }),
-]
-
-export default eslintConfig
+export default nextConfig;
